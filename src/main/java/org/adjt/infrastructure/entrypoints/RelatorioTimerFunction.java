a13e1dbc870ccc631cc3291b748e21dee75e8fb5
mateus barbeiro <mateusgarcia2001@gmail.com>
2026-05-29 20:26:32 -0300

    Add transactional support and timer trigger to RelatorioTimerFunction

--- src/main/java/org/adjt/infrastructure/entrypoints/RelatorioTimerFunction.java
+++ src/main/java/org/adjt/infrastructure/entrypoints/RelatorioTimerFunction.java
@@ -1,14 +1,19 @@
 package org.adjt.infrastructure.entrypoints;
 
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+import jakarta.transaction.Transactional;
+
 public class RelatorioTimerFunction {
 
-//    @Transactional
-//    @FunctionName("GerarRelatorioSemanal")
-//    public void run(
-//            // Expressão CRON: Roda a cada 30 segundos
-//            @TimerTrigger(name = "timerInfo", schedule = "*/30 * * * * *") String timerInfo,
-//            final ExecutionContext context) {
-//
-//        context.getLogger().info("Iniciando geração de relatório semanal...");
-//    }
+    @Transactional
+    @FunctionName("GerarRelatorioSemanal")
+    public void run(
+            // Expressão CRON: Roda a cada 30 segundos
+            @TimerTrigger(name = "timerInfo", schedule = "*/30 * * * * *") String timerInfo,
+            final ExecutionContext context) {
+
+        context.getLogger().info("Iniciando geração de relatório semanal...");
+    }
 }
